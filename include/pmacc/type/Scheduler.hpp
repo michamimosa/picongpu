3cf8905f3b3fb464c4e7269aef257424295f92d2
Michael Sippel <micha@infinitycoding.de>
2019-01-26 14:35:28 +0100

    scheduling policies: make dispatcher the last policy
    The DispatchPolicy potentially removes nodes from the graph so it should be the last policy to be executed during an update cycle

--- include/pmacc/type/Scheduler.hpp
+++ include/pmacc/type/Scheduler.hpp
@@ -29,12 +29,33 @@
 namespace pmacc
 {
 
-using GraphvizPolicy = rmngr::GraphvizWriter< rmngr::DispatchPolicy<rmngr::FIFO>::RuntimeProperty >;
+  template <
+    typename Job
+  >
+  struct PMaccDispatch : rmngr::FIFO<Job>
+  {
+
+    struct Property : rmngr::FIFO<Job>::Property
+    {
+        Property() : dont_schedule_me(false) {}
+        bool dont_schedule_me;
+    };
+
+    void push( Job const & j, Property const & prop = Property() )
+    {
+        if(! prop.dont_schedule_me )
+            this->rmngr::FIFO<Job>::push( j, prop );
+    }
+  };
+
+using GraphvizPolicy = rmngr::GraphvizWriter< rmngr::DispatchPolicy< PMaccDispatch >::RuntimeProperty >;
 using Scheduler = rmngr::SchedulerSingleton<
     boost::mpl::vector<
-        rmngr::DispatchPolicy<rmngr::FIFO>,
         rmngr::ResourceUserPolicy,
-        GraphvizPolicy
+        GraphvizPolicy,
+
+        // dispatcher should always be the last policy
+        rmngr::DispatchPolicy< PMaccDispatch >
     >
 >;
 
