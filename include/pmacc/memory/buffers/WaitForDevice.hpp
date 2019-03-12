43f659427c24aac78c450b35f901943c8098445a
Michael Sippel <micha@infinitycoding.de>
2019-03-12 17:56:47 +0100

    device event system with segfaults

--- include/pmacc/memory/buffers/WaitForDevice.hpp
+++ include/pmacc/memory/buffers/WaitForDevice.hpp
@@ -6,12 +6,85 @@
 #include <rmngr/task.hpp>
 #include <pmacc/tasks/StreamTask.hpp>
 
+#include <memory> // std::addressof()
 #include <csignal>
 #include <sys/mman.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 namespace pmacc
 {
+
+
+namespace waitfordevice
+{  
+    void* page1;
+    void* page2;
+    long PAGE_SIZE;
+    int event_pipe[2];
+    struct sigaction old_sigaction;
+
+    void event_loop()
+    {
+        char v;
+        while( read(event_pipe[0], &v, 1) == 1 )
+        {
+            std::cout << "trigger update from device" << std::endl;
+	    Scheduler::getInstance().policy< rmngr::DispatchPolicy< PMaccDispatch > >().notify();
+	}
+    }
+ 
+    void segv_handler(int signal, siginfo_t* siginfo, void* uap)
+    {
+        if( siginfo->si_addr == page1 )
+	{
+	    mprotect(page1, PAGE_SIZE, PROT_WRITE);
+	    mprotect(page2, PAGE_SIZE, PROT_READ);
+	}
+	else if( siginfo->si_addr == page2 )
+	{
+	    mprotect(page1, PAGE_SIZE, PROT_READ);
+	    mprotect(page2, PAGE_SIZE, PROT_WRITE);
+
+	    char v = 1;
+	    write(event_pipe[1], &v, 1);
+	}
+	else
+	{
+	  // A real SEGV
+	  sigaction(SIGSEGV, &old_sigaction, NULL);
+	}
+    }
+
+    void setup()
+    {
+        PAGE_SIZE = sysconf(_SC_PAGESIZE);
+      /*
+        static sigsegv_dispatcher dispatcher;
+        sigsegv_init (&dispatcher);
+        sigsegv_install_handler (&handler);
+     */
+        struct sigaction act;
+	act.sa_sigaction = &segv_handler;
+	act.sa_flags = SA_SIGINFO;
+        sigaction(SIGSEGV, &act, &old_sigaction);
+
+        pipe( event_pipe );
+	std::thread event_thread(event_loop);
+	event_thread.detach();
+
+	page1 = aligned_alloc( PAGE_SIZE, PAGE_SIZE );
+	page2 = aligned_alloc( PAGE_SIZE, PAGE_SIZE );
+	/*
+	sigsegv_register (&dispatcher, page1, PAGE_SIZE, &segv_handler, &page1 );
+	sigsegv_register (&dispatcher, page2, PAGE_SIZE, &segv_handler, &page2 );
+	*/
+        mprotect( page1, PAGE_SIZE, PROT_READ );
+	mprotect( page2, PAGE_SIZE, PROT_WRITE );
+    }
+
+}
+
 
 struct WaitForDeviceLabel
 {
@@ -46,23 +119,23 @@ private:
         return device_ptr;
     }
 
-    std::atomic_flag* init_clear_flag()
+    int* init_something()
     {
-        std::atomic_flag * device_ptr;
-        std::atomic_flag value = ATOMIC_FLAG_INIT;
-        CUDA_CHECK(cudaMalloc( (void**) &device_ptr, sizeof(std::atomic_flag) ));
-        CUDA_CHECK(cudaMemset( device_ptr, *reinterpret_cast<int*>(&value), sizeof(std::atomic_flag) ));
+        int * device_ptr;
+        int value = 1;
+        CUDA_CHECK(cudaMalloc( (void**) &device_ptr, sizeof(int) ));
+        CUDA_CHECK(cudaMemset( device_ptr, 1, sizeof(int) ));
         return device_ptr;
     }
 
 public:
     void run()
     {
+      ///std::cout << "WAIT FOR DEVICE" << std::endl;
         static State * state_device_ptr = init_state();
-        static std::atomic_flag * flag_device_ptr = init_clear_flag();
+	State volatile * state_host_ptr;
 
-        State volatile * state_host_ptr;
-        std::atomic_flag volatile * flag_host_ptr = &Scheduler::getInstance().uptodate;
+        static int * some_device_ptr = init_something();
 
         // create empty task
         auto res = Scheduler::enqueue_functor(
@@ -74,7 +147,7 @@ public:
                 >().dont_schedule_me = true;
                 s->proto_property< GraphvizPolicy >().label = "on device";
 
-                state_host_ptr = & ( s->runtime_property<rmngr::DispatchPolicy<PMaccDispatch>>().state );
+                state_host_ptr = std::addressof( s->runtime_property<rmngr::DispatchPolicy<PMaccDispatch>>().state );
             }
         );
 
@@ -87,14 +160,23 @@ public:
             this->getCudaStream()
         ));
 
-        // mark graph dirty
-        CUDA_CHECK(cudaMemcpyAsync(
-            const_cast<std::atomic_flag*>(flag_host_ptr),
-            flag_device_ptr,
-            sizeof(std::atomic_flag),
+	// trigger sigsegv
+	CUDA_CHECK(cudaMemcpyAsync(
+	    waitfordevice::page1,
+            some_device_ptr,
+            sizeof(int),
             cudaMemcpyDeviceToHost,
             this->getCudaStream()
         ));
+
+	CUDA_CHECK(cudaMemcpyAsync(
+	    waitfordevice::page2,
+            some_device_ptr,
+            sizeof(int),
+            cudaMemcpyDeviceToHost,
+            this->getCudaStream()
+	));
+
     }
 };
 
