966bcffa8ebf1c1a801f8390e737e91fde345df6
Michael Sippel <micha@infinitycoding.de>
2019-04-11 10:42:24 +0200

    MPI send & receive tasks

---
+++ include/pmacc/communication/MPIReceive.hpp
@@ -0,0 +1,84 @@
+
+#include <pmacc/tasks/MPITask.hpp>
+#include <pmacc/communication/manager_common.hpp>
+#include <pmacc/communication/ICommunicator.hpp>
+#include <pmacc/memory/buffers/Exchange.hpp>
+#include <pmacc/memory/buffers/CopyHostToDevice.hpp>
+#include <pmacc/memory/buffers/CopyDeviceToDevice.hpp>
+
+#include <mpi.h>
+
+namespace pmacc {
+
+namespace communication {
+
+struct MPIReceiveLabel
+{
+    void properties(Scheduler::Schedulable& s)
+    {
+        s.proto_property< rmngr::GraphvizPolicy >().label = "MPI Send";
+    }
+}
+
+template <
+    typename T,
+    size_t T_Dim
+>
+class TaskMPIReceive
+  : public rmngr::Task<
+               TaskReceiveMPI,
+               boost::mpl::vector<
+		   ReceiveTask,
+		   MPITask,
+		   MPIReceiveLabel
+	       >
+           >
+{
+public:
+    TaskReceiveMPI(Exchange<T, T_Dim> & exchange)
+    {
+        this->exchange = &exchange;
+    }
+
+    void run()
+    {
+        MPI_Request * request = Environment<T_DIM>::get()
+	    .EnvironmentController()
+	    .getCommunicator()
+	    .startReceive(
+	        exchange->getExchangeType(),
+		(char*) exchange->getHostBuffer().getBasePointer(),
+		exchange->getHostBuffer().getDataSpace().productOfComponents() * sizeof (TYPE),
+		exchange->getCommunicationTag());
+
+	TaskMPIWait::create( request );
+
+	if (data != nullptr)
+	{
+	    EventDataReceive *rdata = static_cast<EventDataReceive*> (data);
+	    // std::cout<<" data rec "<<rdata->getReceivedCount()/sizeof(TYPE)<<std::endl;
+	    newBufferSize = rdata->getReceivedCount() / sizeof (TYPE);
+	}
+
+        exchange->getHostBuffer().setCurrentSize( newBufferSize );
+	if( exchange->hasDeviceDoubleBuffer() )
+	{
+	    TaskCopyHostToDevice::create(
+	        exchange->getHostBuffer(),
+		exchange->getDeviceDoubleBuffer() );
+	    TaskCopyDeviceToDevice::create(
+		exchange->getDeviceDoubleBuffer(),
+		exchange->getDeviceBuffer());
+	}
+        else
+        {
+            TaskCopyHostToDevice::create(
+                exchange->getHostBuffer(),
+		exchange->getDeviceBuffer());
+        }
+    }
+}
+
+} // namespace communication
+
+} // namespace pmacc
---
+++ include/pmacc/communication/Tasks.hpp
@@ -0,0 +1,68 @@
+
+#pragma once
+
+#include <pmacc/types.hpp>
+#include <pmacc/memory/buffers/Exchange.hpp>
+
+namespace pmacc
+{
+namespace communication
+{
+
+template<
+    typename T,
+    size_t T_Dim
+>
+class SendTask
+{
+protected:
+    Exchange<T, T_Dim> * exchange;
+
+public:
+    void properties(Scheduler::Schedulable& s)
+    {
+        auto& al = s.proto_property< rmngr::ResourceUserPolicy >().access_list;
+	al.push_back( this->exchange->getDeviceBuffer().read() );
+	al.push_back( this->exchange->getDeviceBuffer().size_resource.write() );
+        al.push_back( this->exchange->getHostBuffer().write() );
+	al.push_back( this->exchange->getHostBuffer().size_resource.write() );
+
+        if( this->exchange.hasDeviceDoubleBuffer() )
+        {
+            al.push_back( this->exchange->getDeviceDoubleBuffer().write() );
+            al.push_back( this->exchange->getDeviceDoubleBuffer().size_resource.write() );
+	}
+    }
+}
+
+template<
+    typename T,
+    size_t T_Dim
+>
+class ReceiveTask
+{
+protected:
+    Exchange<T, T_Dim> * exchange;
+
+public:
+    void properties(Scheduler::Schedulable& s)
+    {
+        auto& al = s.proto_property< rmngr::ResourceUserPolicy >().access_list;
+        al.push_back( this->exchange->getDeviceBuffer().write() );
+        al.push_back( this->exchange->getDeviceBuffer().size_resource.write() );
+        al.push_back( this->exchange->getHostBuffer().write() );
+        al.push_back( this->exchange->getHostBuffer().size_resource.write() );
+
+        if( this->exchange->hasDeviceDoubleBuffer() )
+        {
+            al.push_back( this->exchange->getDeviceDoubleBuffer().write() );
+	    al.push_back( this->exchange->getDeviceDoubleBuffer().size_resource.write() );
+        }
+    }
+}
+
+  
+} // namespace communication
+
+} // namespace pmacc
+
