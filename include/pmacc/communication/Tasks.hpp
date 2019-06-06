b9c2d65882e811add2f6d5b0de69b7c8c61974f8
Michael Sippel <m.sippel@hzdr.de>
2019-06-06 10:59:48 +0200

    fix GridBuffer communication

--- include/pmacc/communication/Tasks.hpp
+++ include/pmacc/communication/Tasks.hpp
@@ -22,16 +22,8 @@ public:
     void properties(Scheduler::Schedulable& s)
     {
         auto& al = s.proto_property< rmngr::ResourceUserPolicy >().access_list;
-	al.push_back( this->exchange->getDeviceBuffer().read() );
-	al.push_back( this->exchange->getDeviceBuffer().size_resource.write() );
-        al.push_back( this->exchange->getHostBuffer().write() );
-	al.push_back( this->exchange->getHostBuffer().size_resource.write() );
-
-        if( this->exchange->hasDeviceDoubleBuffer() )
-        {
-            al.push_back( this->exchange->getDeviceDoubleBuffer().write() );
-            al.push_back( this->exchange->getDeviceDoubleBuffer().size_resource.write() );
-	}
+        al.push_back( this->exchange->getHostBuffer().read() );
+	al.push_back( this->exchange->getHostBuffer().size_resource.read() );
 
 	s.proto_property< GraphvizPolicy >().label = "Send";
     }
@@ -50,16 +42,8 @@ public:
     void properties(Scheduler::Schedulable& s)
     {
         auto& al = s.proto_property< rmngr::ResourceUserPolicy >().access_list;
-        al.push_back( this->exchange->getDeviceBuffer().write() );
-        al.push_back( this->exchange->getDeviceBuffer().size_resource.write() );
         al.push_back( this->exchange->getHostBuffer().write() );
         al.push_back( this->exchange->getHostBuffer().size_resource.write() );
-
-        if( this->exchange->hasDeviceDoubleBuffer() )
-        {
-            al.push_back( this->exchange->getDeviceDoubleBuffer().write() );
-	    al.push_back( this->exchange->getDeviceDoubleBuffer().size_resource.write() );
-        }
 
 	s.proto_property< GraphvizPolicy >().label = "Receive";
     }
