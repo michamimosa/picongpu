a0846902f53d8d174325c25b5207d81ca4e372bb
Michael Sippel <michael.sippel@mailbox.tu-dresden.de>
2019-11-14 13:07:53 +0100

    fix mpi tasks
    use polling thread for mpi requests

--- include/pmacc/memory/buffers/GridBuffer.hpp
+++ include/pmacc/memory/buffers/GridBuffer.hpp
@@ -435,10 +435,10 @@ public:
     void communication()
     {
         for (uint32_t i = 0; i < maxExchange; ++i)
+        {
             send( Mask::getMirroredExchangeType(i) );
-
-        for (uint32_t i = 0; i < maxExchange; ++i)
             receive( i );
+        }
     }
 
     void send(uint32_t sendEx)
